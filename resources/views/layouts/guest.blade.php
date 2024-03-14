4a11bdc3e3766184f04e3ebdab5d87f525601bca
a-pb <olowadrian@gmail.com>
2024-03-14 15:02:13 +0100

    chore [PM-14] update templates

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Liczba załadowynych plików") }}: {{ $files->count() }} // TODO: Add translation
+                    {{ __("Liczba załadowynych plików") }}: {{ $files->count() }}
                 </div>
             </div>
         </div>
