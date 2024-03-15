99ea9cffd4fcab822242842f7857432b8da2b492
a-pb <olowadrian@gmail.com>
2024-03-15 08:04:12 +0100

    feat [PM-15] update templates

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("Liczba załadowynych plików") }}: {{ $files->count() }}
+                    {{ __("Rozmiar załadowanych plików") }}: {{ $files->count() }}
                 </div>
             </div>
         </div>
